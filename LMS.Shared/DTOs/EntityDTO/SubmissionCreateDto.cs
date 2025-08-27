75d76a3b6bb01d3082bfcfb474cb3b68f5eccba9
Vilith <vilith44@gmail.com>
2025-08-27 10:54:35 +0200

    Add file upload feature and DTOs for submissions
    Updated SubmissionComponent.razor to support file uploads,
    displaying a loading message and listing previous submissions.
    Integrated SubmissionComponent into CourseList.razor for
    direct access. Introduced SubmissionCreateDto and SubmissionDto
    for managing submission data.

---
+++ LMS.Shared/DTOs/EntityDTO/SubmissionCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.EntityDTO
+{
+    public class SubmissionCreateDto
+    {
+        public Guid DocumentId { get; set; }   // The ID of the document being submitted
+        public string UserId { get; set; } = string.Empty; // The ID of the user submitting the document
+        //public IFormFile File { get; set; } = default!;   // The file being uploaded
+    }
+}
---
+++ LMS.Shared/DTOs/EntityDTO/SubmissionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.EntityDTO
+{
+    public class SubmissionDto
+    {
+        public Guid Id { get; set; }
+        public Guid DocumentId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+        public DateTime SubmittedAt { get; set; }
+    }
+}
